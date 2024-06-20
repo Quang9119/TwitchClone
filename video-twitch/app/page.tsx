7003ab14f1fe7128e60317ee715164a213a91fc8
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-06-20 13:51:12 +0700

    authenticated with clerk

--- video-twitch/app/page.tsx
+++ video-twitch/app/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <Button size={"sm"}>
-      Click me
-    </Button>
-  );
+  return <p>Only authenticated users can see this page</p>;
 }
